c548b814940d8d5a63625e546c3321a86b176336
sauravr09 <sauravr0123@gmail.com>
2026-08-27 16:40:22 -0400

    successfully register a user; handle unique email/username; give JWT token upon registeration

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -18,17 +18,24 @@ router.post('/register', async (req, res) => {
     }
 
     try {
+        const existingUsername = db.prepare(`SELECT id FROM users WHERE username = ?`).get(username);
+        if (existingUsername) {
+            return res.status(409).json({error: "Username is taken"});
+        }
+        const existingEmail = db.prepare(`SELECT id FROM users WHERE email = ?`).get(email);
+        if(existingEmail) {
+            return res.status(409).json({error: "Email is already in use"});
+        }
         const hashedPassword = await bcrypt.hash(password, 8);
         const newUser = db.prepare(`INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)`)
         const result = newUser.run(username, email, hashedPassword);
 
         // give user a first task (CREATE A TASK) once Task DB is created
-        const token = jwt.sign( {id: result.lastInsertRowid}, process.env.JWT_TOKEN! , {expiresIn: '24h'});
+        const token = jwt.sign( {id: result.lastInsertRowid}, process.env.JWT_SECRET! , {expiresIn: '24h'});
         return res.status(201).json({message: 'Successfully registered', token});
-    }
-    catch(error) {
-        if (error instanceof Error){
-            return res.status(503).json({error: error.message});
+    }catch(error) {
+        if (error instanceof Error) {
+            return res.status(503).json({error: error.message})
         }
         return res.status(503).json({error: "Something went wrong"});
     }
--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ import authRoutes from './routes/authRoutes.js'
 initDb();
 
 const app = express(); 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middlewares 
 app.use(express.json()); 
