7a36b9208eb1b758768fde483530666359191231
sauravr09 <sauravr0123@gmail.com>
2026-08-27 15:49:45 -0400

    register endpoint basically done; validate for unique username and email

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -2,8 +2,10 @@ import { db } from "./connection.js";
 
 export function initDb() { 
     db.exec(`
+
         CREATE TABLE IF NOT EXISTS users(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
             email TEXT NOT NULL UNIQUE,
             password_hash TEXT NOT NULL,
             created_at TEXT NOT NULL DEFAULT (datetime('now'))
--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,10 +1,37 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt, {compareSync} from 'bcryptjs';
+import { db } from '../db/connection.js';
+import type { UserRegisterRequestBody } from '../types/user.js';
 
 const router = express.Router();
 
 // Register a user 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
+    const {username, email, password, confirmPassword}: UserRegisterRequestBody = req.body;
     
+    if (!username || !email || !password || !confirmPassword ) {
+        return res.status(400).json({error: "Empty fields"})
+    }
+    if (password !== confirmPassword){
+        return res.status(400).json({error: "Passwords must match"});
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 8);
+        const newUser = db.prepare(`INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)`)
+        const result = newUser.run(username, email, hashedPassword);
+
+        // give user a first task (CREATE A TASK) once Task DB is created
+        const token = jwt.sign( {id: result.lastInsertRowid}, process.env.JWT_TOKEN! , {expiresIn: '24h'});
+        return res.status(201).json({message: 'Successfully registered', token});
+    }
+    catch(error) {
+        if (error instanceof Error){
+            return res.status(503).json({error: error.message});
+        }
+        return res.status(503).json({error: "Something went wrong"});
+    }
 })
 
 export default router;
